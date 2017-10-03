26873adf35ce8b29d3b8c238188e1a4b8e7d2fa8
Sven Jäger <sv_jaeger@gmx.de>
2017-10-03 15:52:04 +0200

    add dummy launchconfig for package

---
+++ build/org.eclipse.cdt.cmake.core/src/org/eclipse/cdt/cmake/core/internal/CMakeCoreBuildLocalNullLaunchDelegate.java
@@ -0,0 +1,18 @@
+package org.eclipse.cdt.cmake.core.internal;
+
+import org.eclipse.cdt.debug.core.launch.CoreBuildLaunchConfigDelegate;
+import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.debug.core.ILaunch;
+import org.eclipse.debug.core.ILaunchConfiguration;
+
+public class CMakeCoreBuildLocalNullLaunchDelegate extends CoreBuildLaunchConfigDelegate {
+
+	@Override
+	public void launch(ILaunchConfiguration configuration, String mode, ILaunch launch, IProgressMonitor monitor)
+			throws CoreException {
+		// Do Nothing
+		
+	}
+
+}
